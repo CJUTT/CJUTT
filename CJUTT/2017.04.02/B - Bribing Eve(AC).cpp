a343a4b8547de86a324d1ca3d87a961b12085fb2
hexsix <hexsixology@gmail.com>
2017-04-03 15:10:53 +0800

    2017.04.03 补题 hexsix

--- CJUTT/2017.04.02/B - Bribing Eve(AC).cpp
+++ CJUTT/2017.04.02/B - Bribing Eve(AC).cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<cstdio>
 #include<algorithm>
 #include<cmath>
 #define maria p[0]
@@ -21,6 +21,10 @@ int cmp2(phone a, phone b) {    // sort by zero point
 	return a._0 < b._0;
 }
 
+int cmp3(phone a, phone b) {    // sort by x2
+	return a.x2 > b.x2;
+}
+
 void init() {
 	top_pos = maxn;
 	low_pos = 1;
@@ -36,9 +40,10 @@ void init() {
 }
 
 int main() {
-	while (cin >> n) {
+	//freopen("in.txt", "r", stdin);
+	while (scanf("%d", &n) != EOF) {
 		for (int i = 0; i < n; i++) {
-			cin >> p[i].x1 >> p[i].x2;
+			scanf("%lf %lf", &p[i].x1, &p[i].x2);
 		}
 		init();
 		// w2 = 0
@@ -50,17 +55,33 @@ int main() {
 		while (pos < n && abs(p[pos].dx1 - 0) <= eps) pos++;
 		if (pos < top_pos) top_pos = pos;
 		if (pos > low_pos) low_pos = pos;
-		// w2 != 0
+
+		// w1 = 0
+		pos = 1;
+		sort(p + 1, p + n, cmp3);
+		while (pos < n && p[pos].dx2 - 0 > eps) pos++;
+		if (pos < top_pos) top_pos = pos;
+		if (pos > low_pos) low_pos = pos;
+		while (pos < n && abs(p[pos].dx2 - 0) <= eps) pos++;
+		if (pos < top_pos) top_pos = pos;
+		if (pos > low_pos) low_pos = pos;
+
+		// w2 != 0 && w1 != 0
 		sort(p + 1, p + n, cmp2);
 		pos = 1;
 		int big = 0, small = 0, equal = 0, flag = 1;
 		for (int i = 1; i < n; i++) {
-			if (0 - p[i]._0 > eps || abs(0 - p[i]._0) < eps) {
+			if (0 - p[i]._0 > eps || abs(0 - p[i]._0) <= eps) {
 				if (p[i].dx1 - 0 > eps) {
 					big++;
 				}
 				else if (abs(p[i].dx1 - 0) <= eps) {
-					equal++;
+					if (abs(p[i].dx2 - 0) <= eps)
+						equal++;
+					else if (p[i].dx2 - 0 > eps)
+						big++;
+					else
+						small++;
 				}
 				else {
 					small++;
@@ -82,33 +103,36 @@ int main() {
 		if (big + 1 < top_pos) top_pos = big + 1;;
 		if (big + equal + 1 > low_pos) low_pos = big + equal + 1;
 
-		for (int i = flag; i < n; i++) {
+		for (int i = flag; i < n;) {
+			int temp_equal = 1, temp_big = 0, temp_small = 0;
 			if (p[i].dx1 - 0 > eps) {
+				temp_big++;
 				small--;
-				equal++;
-				if (big + 1 < top_pos) top_pos = big + 1;;
-				if (big + equal + 1 > low_pos) low_pos = big + equal + 1;
-				equal--;
-				big++;
-				if (big + 1 < top_pos) top_pos = big + 1;;
-				if (big + equal + 1 > low_pos) low_pos = big + equal + 1;
-			}
-			else if (abs(p[i].dx1 - 0) <= eps) {
-				return 1;
 			}
 			else {
+				temp_small++;
 				big--;
-				equal++;
-				if (big + 1 < top_pos) top_pos = big + 1;;
-				if (big + equal + 1 > low_pos) low_pos = big + equal + 1;
-				equal--;
-				small++;
-				if (big + 1 < top_pos) top_pos = big + 1;;
-				if (big + equal + 1 > low_pos) low_pos = big + equal + 1;
 			}
+			while (i + temp_equal < n && abs(p[i]._0 - p[i + temp_equal]._0) <= eps) {
+				if (p[i + temp_equal].dx1 - 0 > eps) {
+					temp_big++;
+					small--;
+				}
+				else {
+					temp_small++;
+					big--;
+				}
+				temp_equal++;
+			}
+			if (big + 1 < top_pos) top_pos = big + 1;;
+			if (big + equal + temp_equal + 1 > low_pos) low_pos = big + equal + temp_equal + 1;
+
+			big += temp_big;
+			small += temp_small;
+			i += temp_equal;
 		}
 
-		cout << top_pos << " " << low_pos << endl;
+		printf("%d %d\n", top_pos, low_pos);
 	}
 	return 0;
 }
